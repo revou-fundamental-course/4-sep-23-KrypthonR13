2e8b527e93831b59dcf0036a1f504f35b751cf6e
RockProgram <105757434+KrypthonR13@users.noreply.github.com>
2023-09-12 21:28:57 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const hasilL = document.getElementById("hasilLuas");
+const hasilK = document.getElementById("hasilKeliling");
+
+function hitungLuas() {
+  const sisi = parseInt(document.getElementById("luasSisi").value);
+  if (isNaN(sisi) || sisi <= 0) {
+    alert("Masukkan bilangan bulat 0 - 9.");
+    return;
+  }
+  const luas = sisi * sisi;
+  hasilL.innerHTML = `
+        <p>Hasil :</p>
+        <p>${luas} cm²</p>
+    `;
+}
+
+function hitungKeliling() {
+  const sisi = parseInt(document.getElementById("kelilingSisi").value);
+  if (isNaN(sisi) || sisi <= 0) {
+    alert("Masukkan bilangan bulat 0 - 9.");
+    return;
+  }
+  const keliling = 4 * sisi;
+  hasilK.innerHTML = `
+        <p>Hasil :</p>
+        <p>${keliling} cm</p>
+    `;
+}
+
+document.getElementById("hitungLuas").addEventListener("click", hitungLuas);
+document
+  .getElementById("hitungKeliling")
+  .addEventListener("click", hitungKeliling);
