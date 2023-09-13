0919d4c84d3043be98b61b3c62b607edb7db2d71
RHTech19 <105757434+KrypthonR13@users.noreply.github.com>
2023-09-13 20:19:06 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ const hasilK = document.getElementById("hasilKeliling");
 
 function hitungLuas() {
   const sisi = parseInt(document.getElementById("luasSisi").value);
-  if (isNaN(sisi) || sisi <= 0) {
-    alert("Jumlah sisi harus bilangan positif dan tidak boleh 0.");
+  if (isNaN(sisi) || sisi <= 0 || sisi == "") {
+    alert("Jumlah sisi harus bilangan positif dan tidak boleh kosong atau 0.");
     return;
   }
   const luas = sisi * sisi;
@@ -16,8 +16,8 @@ function hitungLuas() {
 
 function hitungKeliling() {
   const sisi = parseInt(document.getElementById("kelilingSisi").value);
-  if (isNaN(sisi) || sisi <= 0) {
-    alert("Jumlah sisi harus bilangan positif dan tidak boleh 0.");
+  if (isNaN(sisi) || sisi <= 0 || sisi == "") {
+    alert("Jumlah sisi harus bilangan positif dan tidak boleh kosong atau 0.");
     return;
   }
   const keliling = 4 * sisi;
